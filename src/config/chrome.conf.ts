9de41b488bd64da272ee0e07ec762913bf5b9658
igorgo <igorgoncharua@gmail.com>
2019-11-14 17:34:26 +0100

    chrome driver version was updated

--- src/config/chrome.conf.ts
+++ src/config/chrome.conf.ts
@@ -22,6 +22,6 @@ export const config = _.merge(computersBaseConfig, {
       }
     }
   },
-  chromeDriver: ('../../../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_74.0.3729.6'
+  chromeDriver: ('../../../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_78.0.3904.70'
     + (process.platform.indexOf('win') === 0 ? '.exe' : ''))
 });
